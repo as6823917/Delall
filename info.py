93ba9fbd4c5da8c015f4576f7f9c745807952124
as6823917 <66206385+as6823917@users.noreply.github.com>
2022-02-11 10:39:59 +0530

    Delete info.py

--- info.py
+++
@@ -1,9 +0,0 @@
-import os
-from os import environ
-
-API_ID = int(os.environ.get("API_ID", 12345))
-API_HASH = os.environ.get("API_HASH", None)
-SESSION = os.environ.get("SESSION")
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
-ADMINS = set(int(x) for x in os.environ.get("ADMINS", "1914868167").split())
-GROUPS = [int(admin) for admin in environ.get("GROUPS", "").split()]
