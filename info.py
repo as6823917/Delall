93e62ad366910358bb523c73348a61a557e8530d
as6823917 <66206385+as6823917@users.noreply.github.com>
2022-02-11 10:38:19 +0530

    Update info.py

--- info.py
+++ info.py
@@ -6,5 +6,4 @@ API_HASH = os.environ.get("API_HASH", None)
 SESSION = os.environ.get("SESSION")
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 ADMINS = set(int(x) for x in os.environ.get("ADMINS", "1914868167").split())
-TIME = os.environ.get("TIME", None)
 GROUPS = [int(admin) for admin in environ.get("GROUPS", "").split()]
